d812c61478491799d5f70c7ead205324bec45f9f
Colin <cpireyre@student.42.fr>
2022-01-26 19:11:50 +0100

    Working on recursive version

--- integer-to-english-words.py
+++ integer-to-english-words.py
@@ -33,54 +33,71 @@ numerals = {0: "Zero",
         10**6: "Million",
         10**9: "Billion"}
 
-# Assumes num < 1000
-def numAnalysis(num):
+# Laborious version below:
 
-    hundreds = num // 10**2
-    tens = (num % 10**2) // 10
-    units = num % 10
+## Assumes num < 1000
+#def numAnalysis(num):
+#
+#    hundreds = num // 10**2
+#    tens = (num % 10**2) // 10
+#    units = num % 10
+#
+#    ret = []
+#    if hundreds:
+#        ret.append(hundreds)
+#        ret.append(100)
+#    if 1 <= tens < 2:
+#        ret.append(num % 10**2)
+#        return ret
+#    elif tens >= 2:
+#        ret.append(tens * 10)
+#    if units:
+#        ret.append(units)
+#    
+#    return ret
+#
+#def num2String(lnum):
+#    return " ".join(map(lambda n: numerals[n], lnum))
+#
+## How many billions? How many millions? How many thousands?
+#def numberToWords(num):
+#
+#    if num <= 20:
+#        return numerals[num]
+#
+#    ret = []
+#    b = num // 10**9
+#    if b:
+#        ret += (numAnalysis(b))
+#        ret += [10**9]
+#    m = (num % 10**9) // 10**6
+#    if m:
+#        ret += (numAnalysis(m))
+#        ret += [10**6]
+#    k = (num % 10**6) // 10**3
+#    if k:
+#        ret += numAnalysis(k)
+#        ret += [10**3]
+#    rest = num % 10**3
+#    if rest:
+#        ret += (numAnalysis(rest))
+#    print(ret)
+#    return(num2String(ret))
 
-    ret = []
-    if hundreds:
-        ret.append(hundreds)
-        ret.append(100)
-    if 1 <= tens < 2:
-        ret.append(num % 10**2)
-        return ret
-    elif tens >= 2:
-        ret.append(tens * 10)
-    if units:
-        ret.append(units)
-    
-    return ret
-
-def num2String(lnum):
-    return " ".join(map(lambda n: numerals[n], lnum))
-
-# How many billions? How many millions? How many thousands?
+# Slick recursive version below:
+from math import log10
+logsOfInterest = [9, 6, 3, 2, 1]
 def numberToWords(num):
-
+    print(num)
+    if num <= 99:
+        return numerals[10 * (num // 10)] + " " + numerals[num % 10]
     if num <= 20:
         return numerals[num]
-
-    ret = []
-    b = num // 10**9
-    if b:
-        ret += (numAnalysis(b))
-        ret += [10**9]
-    m = (num % 10**9) // 10**6
-    if m:
-        ret += (numAnalysis(m))
-        ret += [10**6]
-    k = (num % 10**6) // 10**3
-    if k:
-        ret += numAnalysis(k)
-        ret += [10**3]
-    rest = num % 10**3
-    if rest:
-        ret += (numAnalysis(rest))
-    print(ret)
-    return(num2String(ret))
+    p = log10(num)
+    k = max(filter(lambda x: x < p, logsOfInterest))
+    return numerals[num // 10**k] + \
+            " " + numerals[10**k] + \
+            " " + numberToWords(num % 10**k)
 
 def runTests(f, T):
     for t in T:
