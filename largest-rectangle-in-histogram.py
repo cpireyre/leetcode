a08bbb5bf14d26629190928193b0dfe9f3b69948
Colin <cpireyre@student.42.fr>
2022-01-29 23:46:16 +0100

    Couldn't get this one today

--- largest-rectangle-in-histogram.py
+++ largest-rectangle-in-histogram.py
@@ -29,32 +29,48 @@
 #print(splits)
 
 from typing import *
-from functools import cache
+#from functools import cache
 # New plan: for a given slice, compute min and area and cache the result
-def largestRectangleArea(heights: List[int]) -> int:
-
-    @cache
-    def recursiveSliceMin(start: int, end: int) -> int:
-        if start == end:
-            return heights[start]
-        else:
-            return min(heights[start], recursiveSliceMin(start + 1, end))
-
-    L = len(heights)
-    lowerBound = L * min(heights)
-    for sliceLength in reversed(range(L)):
-        sliceGen = ((1 + sliceLength) * recursiveSliceMin(k, k + sliceLength) for k in range(L - sliceLength))
-        localMax = max(sliceGen)
-        if localMax > lowerBound:
-            lowerBound = localMax
-
-    return lowerBound
+#def largestRectangleArea(heights: List[int]) -> int:
+#
+#    @cache
+#    def recursiveSliceMin(start: int, end: int) -> int:
+#        if start == end:
+#            return heights[start]
+#        else:
+#            return min(heights[start], recursiveSliceMin(start + 1, end))
+#
+#    L = len(heights)
+#    lowerBound = max(heights)
+#    for sliceLength in range(1, L):
+#        sliceGen = ((1 + sliceLength) * recursiveSliceMin(k, k + sliceLength) for k in range(L - sliceLength))
+#        localMax = max(sliceGen)
+#        if localMax > lowerBound:
+#            lowerBound = localMax
+#
+#    return lowerBound
 # Memory limit exceeded on an array full of 1s. recursiveSliceMin blew up
 # It shouldn't have though?
 
-heights = [2,1,5,6,2,3]
-print(largestRectangleArea(heights))
-heights = [2,4]
-print(largestRectangleArea(heights))
-heights = [100000]
-print(largestRectangleArea(heights))
+#heights = [2,1,5,6,2,3]
+#print(largestRectangleArea(heights))
+#heights = [2,4]
+#print(largestRectangleArea(heights))
+#heights = [100000]
+#print(largestRectangleArea(heights))
+
+# I couldn't get this one. Copy pasting from Leetcode.
+# One has to use what is called a "monotonic stack"
+
+def largestRectangleArea(self, bars: List[int]) -> int:
+	st, res = [], 0
+	for bar in bars + [-1]: # add -1 to have an additional iteration
+		step = 0
+		while st and st[-1][1] >= bar:
+			w, h = st.pop()
+			step += w
+			res = max(res, step * h)
+
+		st.append((step + 1, bar))
+
+	return res
