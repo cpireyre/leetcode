f6dbed92ac4218b69550453f2b7515ddd54ee7a4
Colin <cpireyre@student.42.fr>
2022-02-14 08:26:28 +0100

    One liner Runtime: 55 ms, faster than 50.62% of Python3 online submissions for Maximum Depth of Binary Tree. Memory Usage: 16.3 MB, less than 31.05% of Python3 online submissions for Maximum Depth of Binary Tree.

--- maximum-depth-of-binary-tree.py
+++ maximum-depth-of-binary-tree.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# BFS ver
 def maxDepth(root: Optional[TreeNode]) -> int:
     maxDepth, q = 0, [root]
     if not root: return 0
@@ -19,3 +20,11 @@ def maxDepth(root: Optional[TreeNode]) -> int:
     return maxDepth
 #Runtime: 63 ms, faster than 37.30% of Python3 online submissions for Maximum Depth of Binary Tree.
 #Memory Usage: 15.4 MB, less than 86.50% of Python3 online submissions for Maximum Depth of Binary Tree.
+
+# DFS one liner
+def maxDepth(root: Optional[TreeNode]) -> int:
+    return 0 if not root else max(1 + self.maxDepth(root.left), 1 + self.maxDepth(root.right))
+    # return 0 if not root else (l if (l := 1 + self.maxDepth(root.left)) > (r := 1 + self.maxDepth(root.right)) else r)
+    # turns out assignments in returns are even worse than overhead of recursively calling max()
+# Runtime: 55 ms, faster than 50.62% of Python3 online submissions for Maximum Depth of Binary Tree.
+# Memory Usage: 16.3 MB, less than 31.05% of Python3 online submissions for Maximum Depth of Binary Tree.
