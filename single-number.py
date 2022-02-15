1166069d0e3f0520c0ebe757f4fe49ff978baf2b
Colin <cpireyre@student.42.fr>
2022-02-15 08:14:37 +0100

    MLE with basic tail recursion lol

--- single-number.py
+++ single-number.py
@@ -6,11 +6,16 @@ from collections import Counter
 #Memory Usage: 16.9 MB, less than 6.05% of Python3 online submissions for Single Number.
 
 # reduce XOR ver I stole from Leetcode
-from functools import reduce
-def singleNumber(nums):
-    return reduce(lambda a, b: a^b, nums)
+# from functools import reduce
+# def singleNumber(nums):
+#     return reduce(lambda a, b: a^b, nums)
 # Runtime: 195 ms, faster than 49.33% of Python3 online submissions for Single Number.
 # Memory Usage: 16.8 MB, less than 30.94% of Python3 online submissions for Single Number.
+
+# Meme tail recurse ver
+def singleNumber(nums):
+    return 0 if not nums else nums[0] ^ singleNumber(nums[1:])
+# Memory limit exceeded lol
 
 nums = [4,1,2,1,2]
 print(singleNumber(nums))
