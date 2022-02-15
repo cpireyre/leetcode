4c2e3a93ee3d23f926769ac918b43ee9566c0369
Colin <cpireyre@student.42.fr>
2022-02-15 08:12:14 +0100

    Runtime: 195 ms, faster than 49.33% of Python3 online submissions for Single Number. Memory Usage: 16.8 MB, less than 30.94% of Python3 online submissions for Single Number.

--- single-number.py
+++ single-number.py
@@ -1,8 +1,16 @@
 from collections import Counter
-def singleNumber(nums):
-    return Counter(nums).most_common()[-1][0]
+# Counter ver
+# def singleNumber(nums):
+#     return Counter(nums).most_common()[-1][0]
 #Runtime: 145 ms, faster than 68.35% of Python3 online submissions for Single Number.
 #Memory Usage: 16.9 MB, less than 6.05% of Python3 online submissions for Single Number.
+
+# reduce XOR ver I stole from Leetcode
+from functools import reduce
+def singleNumber(nums):
+    return reduce(lambda a, b: a^b, nums)
+# Runtime: 195 ms, faster than 49.33% of Python3 online submissions for Single Number.
+# Memory Usage: 16.8 MB, less than 30.94% of Python3 online submissions for Single Number.
 
 nums = [4,1,2,1,2]
 print(singleNumber(nums))
