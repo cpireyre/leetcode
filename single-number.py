11d1bb56f7f1dc363b429cd4cb4fb1a0c3997f50
Colin <cpireyre@student.42.fr>
2022-02-15 09:33:43 +0100

    Runtime: 167 ms, faster than 60.44% of Python3 online submissions for Single Number. Memory Usage: 16.5 MB, less than 96.68% of Python3 online submissions for Single Number.

--- single-number.py
+++ single-number.py
@@ -13,9 +13,30 @@ from collections import Counter
 # Memory Usage: 16.8 MB, less than 30.94% of Python3 online submissions for Single Number.
 
 # Meme tail recurse ver
-def singleNumber(nums):
-    return 0 if not nums else nums[0] ^ singleNumber(nums[1:])
+# def singleNumber(nums):
+#     return 0 if not nums else nums[0] ^ singleNumber(nums[1:])
 # Memory limit exceeded lol
+
+# Primitive bare metal ver
+def singleNumber(nums):
+    acc = 0
+    for n in nums:
+        acc ^= n
+    return acc
+# Runtime: 241 ms, faster than 28.20% of Python3 online submissions for Single Number.
+# Memory Usage: 16.7 MB, less than 30.94% of Python3 online submissions for Single Number.
+# Where is all that memory going???
+
+# Disgusting mutate everything in place ver
+def singleNumber(nums):
+    i = 1
+    while i < len(nums):
+        nums[i] ^= nums[i - 1]
+        i += 1
+    return nums[-1]
+# Runtime: 167 ms, faster than 60.44% of Python3 online submissions for Single Number.
+# Memory Usage: 16.5 MB, less than 96.68% of Python3 online submissions for Single Number.
+# lol lmao
 
 nums = [4,1,2,1,2]
 print(singleNumber(nums))
