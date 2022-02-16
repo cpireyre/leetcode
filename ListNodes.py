cf9690f85eb4e41db97f6d1d01a657143c3510a8
Colin <cpireyre@student.42.fr>
2022-02-16 11:33:52 +0100

    Extracted ListNode class

---
+++ ListNodes.py
@@ -0,0 +1,29 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def listMap(self, func):
+        func(self.val)
+        if self.next:
+            self.next.listMap(func)
+
+
+def fromArray(nums):
+    if nums:
+        ret = ListNode(nums[0])
+        tmp = ret
+        for n in nums[1:]:
+            tmp.next = ListNode(n)
+            tmp = tmp.next
+        return ret
+    else:
+        return []
+
+
+def toArray(head):
+    res = []
+    while head:
+        res.append(head.val)
+        head = head.next
+    return res
--- swap-nodes-in-pairs.py
+++ swap-nodes-in-pairs.py
@@ -1,35 +1,5 @@
 from typing import *
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-    def listMap(self, func):
-        func(self.val)
-        if self.next:
-            self.next.listMap(func)
-
-
-def fromArray(nums):
-    if nums:
-        ret = ListNode(nums[0])
-        tmp = ret
-        for n in nums[1:]:
-            tmp.next = ListNode(n)
-            tmp = tmp.next
-        return ret
-    else:
-        return []
-
-
-def toArray(head):
-    res = []
-    while head:
-        res.append(head.val)
-        head = head.next
-    return res
-
-
+from ListNodes import *
 def swapPairs(head: Optional[ListNode]) -> Optional[ListNode]:
     if not head:
         return None
