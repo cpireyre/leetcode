c55e3930af2d6523c204952d0c3f7eee36261629
Colin <cpireyre@student.42.fr>
2022-02-20 10:18:58 +0100

    One liner lol Runtime: 110 ms, faster than 67.27% of Python3 online submissions for Remove Covered Intervals. Memory Usage: 14.4 MB, less than 86.23% of Python3 online submissions for Remove Covered Intervals.

--- remove-covered-intervals.py
+++ remove-covered-intervals.py
@@ -1,29 +1,7 @@
+# Atrocious one liner to scare the normies
 def removeCoveredIntervals(intervals):
-    def intKey(i):
-        return (i[0], -i[1])
-
-    def covers(a, b):
-        return a[0] <= b[0] < b[1] <= a[1]
-
     def removeRecur(ts):
-        if not ts:
-            return 0
-        return 1 + removeRecur([i for i in ts[1:] if not covers(ts[0], i)])
-
-    return removeRecur(sorted((tuple(i) for i in intervals), key=intKey))
-# Runtime: 96 ms, faster than 85.97% of Python3 online submissions for Remove Covered Intervals.
-# Memory Usage: 14.8 MB, less than 59.22% of Python3 online submissions for Remove Covered Intervals.
-
-
-intervals = [[1, 4], [3, 6], [2, 8]]
-print(removeCoveredIntervals(intervals))
-intervals = [
-    [34335, 39239],
-    [15875, 91969],
-    [29673, 66453],
-    [53548, 69161],
-    [40618, 93111],
-]
-print(removeCoveredIntervals(intervals))
-intervals = [[1, 2], [1, 4], [3, 4]]
-print(removeCoveredIntervals(intervals))
+        return 1 + removeRecur([i for i in ts[1:] if not ts[0][0] <= i[0] < i[1] <= ts[0][1]]) if ts else 0
+    return removeRecur(sorted(intervals, key=lambda i: (i[0], -i[1])))
+# Runtime: 110 ms, faster than 67.27% of Python3 online submissions for Remove Covered Intervals.
+# Memory Usage: 14.4 MB, less than 86.23% of Python3 online submissions for Remove Covered Intervals.
