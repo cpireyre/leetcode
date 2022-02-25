a51ceac0c1a166478b87e3e078887f3ebb983603
Colin <cpireyre@student.42.fr>
2022-02-25 11:55:34 +0100

    one liner lol Runtime: 32 ms, faster than 83.77% of Python3 online submissions for Compare Version Numbers. Memory Usage: 13.9 MB, less than 68.52% of Python3 online submissions for Compare Version Numbers.

--- compare-version-numbers.py
+++ compare-version-numbers.py
@@ -1,16 +1,12 @@
-def compareVersions(version1, version2):
-        parse = lambda ver: [int(s) for s in ver.split(".")]
-        version1, version2 = parse(version1), parse(version2)
-        lv1, lv2 = len(version1), len(version2)
-        version1, version2 = version1 + [0]*(lv2 - lv1), version2 +[0]*(lv1 - lv2)
-        for a, b in zip(version1, version2):
-            if a > b:
-                return 1
-            elif a < b:
-                return -1
-        return 0
-# Runtime: 38 ms, faster than 62.44% of Python3 online submissions for Compare Version Numbers.
+# One liner to scare the normies
+from itertools import zip_longest
+
+
+def compareVersions(v1, v2):
+    return next( ( (a - b) // abs(a - b) for a, b in zip_longest( map(int, v1.split(".")), map(int, v2.split(".")), fillvalue=0) if a != b), 0,)
+# Runtime: 32 ms, faster than 83.77% of Python3 online submissions for Compare Version Numbers.
 # Memory Usage: 13.9 MB, less than 68.52% of Python3 online submissions for Compare Version Numbers.
+
 
 version1 = "1.01"
 version2 = "1.001"
