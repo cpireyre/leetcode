f8331f798cd5169448c86f8db6961186117590c4
Colin <cpireyre@student.42.fr>
2022-03-02 15:08:08 +0100

    Runtime: 44 ms, faster than 50.22% of Python3 online submissions for Number of Steps to Reduce a Number to Zero. Memory Usage: 13.8 MB, less than 98.14% of Python3 online submissions for Number of Steps to Reduce a Number to Zero.

---
+++ number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,7 @@
+def numberOfSteps(n):
+    return n if n <= 3 else sum((n >= 1<<i) * (1 + (not not (n & 1<<i))) for i in range(25)) - 1
+# Runtime: 44 ms, faster than 50.22% of Python3 online submissions for Number of Steps to Reduce a Number to Zero.
+# Memory Usage: 13.8 MB, less than 98.14% of Python3 online submissions for Number of Steps to Reduce a Number to Zero.
+
+from sys import argv
+print(numberOfSteps(int(argv[1])))
