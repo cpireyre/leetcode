8f716e376ccf514129618e0e03f0513d205faea7
Colin <cpireyre@student.42.fr>
2022-03-02 15:11:24 +0100

    turns out bit_count() and bit_length() are things

--- number-of-steps-to-reduce-a-number-to-zero.py
+++ number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,3 +1,4 @@
+# next time just use .bit_length() and .bit_count() 🙄
 def numberOfSteps(n):
     return n if n <= 3 else sum((n >= 1<<i) * (1 + (not not (n & 1<<i))) for i in range(25)) - 1
 # Runtime: 44 ms, faster than 50.22% of Python3 online submissions for Number of Steps to Reduce a Number to Zero.
