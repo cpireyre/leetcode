da436f550206116a3ea1bbdb298936edf7474bc8
Colin <cpireyre@student.42.fr>
2022-03-03 14:37:18 +0100

    one liner to scare the normies Runtime: 55 ms, faster than 50.00% of Python3 online submissions for Arithmetic Slices. Memory Usage: 14.1 MB, less than 97.59% of Python3 online submissions for Arithmetic Slices.

--- arithmetic-slices.py
+++ arithmetic-slices.py
@@ -1,9 +1,8 @@
 from itertools import pairwise, groupby
 def numberOfArithmeticSlices(ns):
-    arithmetics = (len(list(g)) - 1 for _, g in groupby(high - low for low, high in pairwise(ns)))
-    return sum((n**2 + n) >> 1 for n in arithmetics if n >= 1)
-# Runtime: 63 ms, faster than 31.69% of Python3 online submissions for Arithmetic Slices.
-# Memory Usage: 14.2 MB, less than 53.58% of Python3 online submissions for Arithmetic Slices.
+    return sum((n**2 + n) >> 1 for n in (len(list(g)) - 1 for _, g in groupby(high - low for low, high in pairwise(ns))) if n >= 1)
+# Runtime: 55 ms, faster than 50.00% of Python3 online submissions for Arithmetic Slices.
+# Memory Usage: 14.1 MB, less than 97.59% of Python3 online submissions for Arithmetic Slices.
 
 ns = [1,2,3,4,5]
 print(numberOfArithmeticSlices(ns))
