a7455e0dbe42bc0729d8b87d88086acef847c1df
Colin <cpireyre@student.42.fr>
2022-03-20 10:45:17 +0100

    Runtime: 1216 ms, faster than 80.89% of Python3 online submissions for Minimum Domino Rotations For Equal Row. Memory Usage: 15.1 MB, less than 77.02% of Python3 online submissions for Minimum Domino Rotations For Equal Row.

--- minimum-domino-rotations-for-equal-row.py
+++ minimum-domino-rotations-for-equal-row.py
@@ -1,17 +1,19 @@
 from itertools import product
+from collections import Counter
 def minDominoRotations(tops, bottoms):
     def count(tops, bottoms):
-        ns = (1, 2, 3, 4, 5, 6)
-        res = {n: (0, 0) for n in ns}
-        for n, (top, bottom) in product(ns, zip(tops, bottoms)):
-            x, y = res[n]
-            x += (top == n)
-            y += (bottom == n and top != n)
-            res[n] = (x, y)
-        return min((min(a, b) for a, b in res.values() if a + b == len(tops)), default=-1)
-    return min(count(tops, bottoms), count(bottoms, tops))
-# Runtime: 2673 ms, faster than 5.02% of Python3 online submissions for Minimum Domino Rotations For Equal Row.
-# Memory Usage: 16.5 MB, less than 5.77% of Python3 online submissions for Minimum Domino Rotations For Equal Row.
+        count, val = max((v, k) for k, v in Counter(tops).items())
+        if count + sum(a != val and b == val for a, b in zip(tops, bottoms)) >= len(tops):
+            return len(tops) - count
+        else:
+            return -1
+    t, b = count(tops, bottoms), count(bottoms, tops)
+    if t == -1 or b == -1:
+        return t if b == -1 else b
+    else:
+        return min(t, b)
+# Runtime: 1216 ms, faster than 80.89% of Python3 online submissions for Minimum Domino Rotations For Equal Row.
+# Memory Usage: 15.1 MB, less than 77.02% of Python3 online submissions for Minimum Domino Rotations For Equal Row.
 
 tops    = [2,1,2,4,2,2]
 bottoms = [5,2,6,2,3,2]
@@ -21,4 +23,10 @@ bottoms = [3,6,3,3,4]
 print(minDominoRotations(tops, bottoms))
 tops    = [1,2,1,1,1,2,2,2]
 bottoms = [2,1,2,2,2,2,2,2]
+print(minDominoRotations(tops, bottoms))
+tops    = [1,1,1,1,1,1,1]
+bottoms = [1,1,1,1,1,1,1]
+print(minDominoRotations(tops, bottoms))
+tops    = [1,2,3,4,5]
+bottoms = [6,6,6,6,6]
 print(minDominoRotations(tops, bottoms))
