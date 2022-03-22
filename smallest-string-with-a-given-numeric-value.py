fd34e0b82c7b29627d7bfbd92ada2ae48c77fc92
Colin <cpireyre@student.42.fr>
2022-03-22 17:07:40 +0100

    Runtime: 72 ms, faster than 81.57% of Python3 online submissions for Smallest String With A Given Numeric Value. Memory Usage: 14.9 MB, less than 87.56% of Python3 online submissions for Smallest String With A Given Numeric Value.

--- smallest-string-with-a-given-numeric-value.py
+++ smallest-string-with-a-given-numeric-value.py
@@ -1,15 +1,23 @@
-def getSmallestString(n, k):
-    res = []
-    while n and k:
-        n -= 1
-        diff = 26 if k - n > 26 else k - n
-        k -= diff
-        res.append(diff)
-    return "".join(chr(ord('a') + c - 1) for c in reversed(res))
+# def getSmallestString(n, k):
+#     res = []
+#     while n and k:
+#         n -= 1
+#         diff = 26 if k - n > 26 else k - n
+#         k -= diff
+#         res.append(diff)
+#     return "".join(chr(ord('a') + c - 1) for c in reversed(res))
 # Runtime: 1016 ms, faster than 37.79% of Python3 online submissions for Smallest String With A Given Numeric Value.
 # Memory Usage: 16.5 MB, less than 7.83% of Python3 online submissions for Smallest String With A Given Numeric Value.
 
-n, k = 3, 27
-# n, k = 5, 73
+def getSmallestString(n, k):
+    q, r = divmod(k - n - 1, 25)
+    return (n - q - 1) * 'a' + chr(97 + r + 1) + 'z' * q
+# Runtime: 72 ms, faster than 81.57% of Python3 online submissions for Smallest String With A Given Numeric Value.
+# Memory Usage: 14.9 MB, less than 87.56% of Python3 online submissions for Smallest String With A Given Numeric Value.
+
+
+
+n, k = 3, 27 # "aay"
+n, k = 5, 73 # "aaszz"
 # n, k = 100000, 26 * 100000
 print(getSmallestString(n, k))
