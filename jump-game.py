ec422a9ab414d704201be038a43802906562ec77
Colin <cpireyre@student.42.fr>
2022-04-04 13:06:17 +0200

    Runtime: 741 ms, faster than 38.34% of Python3 online submissions for Jump Game. Memory Usage: 15.1 MB, less than 83.79% of Python3 online submissions for Jump Game.

--- jump-game.py
+++ jump-game.py
@@ -1,18 +1,25 @@
+from itertools import accumulate
+
+
 def canJump(ns):
     goal = len(ns) - 1
-    fuel = -1
-    for station, tank in enumerate(ns):
-        fuel = tank if tank >= fuel - 1 else fuel - 1
+
+    def step(a, b):
+        return b if b >= a - 1 else a - 1
+
+    for station, fuel in enumerate(accumulate(ns, step)):
         if station + fuel >= goal:
             return True
         elif fuel <= 0:
             return False
-# Runtime: 492 ms, faster than 92.61% of Python3 online submissions for Jump Game.
-# Memory Usage: 15.4 MB, less than 20.93% of Python3 online submissions for Jump Game.
+# Runtime: 741 ms, faster than 38.34% of Python3 online submissions for Jump Game.
+# Memory Usage: 15.1 MB, less than 83.79% of Python3 online submissions for Jump Game.
 
-ns = [2,3,1,1,4] # True
+
+
+ns = [2, 3, 1, 1, 4]  # True
 print(canJump(ns))
-ns = [3,2,1,0,4] # False
+ns = [3, 2, 1, 0, 4]  # False
 print(canJump(ns))
-ns = [0] # True
+ns = [0]  # True
 print(canJump(ns))
