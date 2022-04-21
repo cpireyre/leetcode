b2ea7b20ddfe617c46a431e400b0774be8d5a552
Colin <cpireyre@student.42.fr>
2022-04-21 10:26:49 +0200

    Runtime: 312 ms, faster than 48.75% of Python3 online submissions for Design HashSet. Memory Usage: 21.4 MB, less than 21.37% of Python3 online submissions for Design HashSet.

--- design-hashset.py
+++ design-hashset.py
@@ -1,15 +1,44 @@
 class MyHashSet:
 
     def __init__(self):
-        self.mem = [False for _ in range(10**6 + 2)]
+        self.primes = list(reversed([
+            53,
+            97,
+            193,
+            389,
+            769,
+            1543,
+            3079,
+            6151,
+            12289,
+            24593,
+            49157,
+            98317,
+            196613,
+            ]))
+        self.size = self.primes.pop()
+        self.mem = [None for _ in range(self.size + 1)]
 
     def add(self, key: int) -> None:
-        self.mem[key] = True
+        if not self.contains(key):
+            while self.mem[key % self.size] != None:
+                self.rehash()
+            self.mem[key % self.size] = key
+
+    def rehash(self):
+        vals = [v for v in self.mem if v]
+        self.size = self.primes.pop()
+        self.mem = [None for _ in range(self.size + 1)]
+        for v in vals:
+            self.add(v)
 
     def remove(self, key: int) -> None:
-        self.mem[key] = False
+        if self.contains(key):
+            self.mem[key % self.size] = None
 
     def contains(self, key: int) -> bool:
-        return self.mem[key]
-# Runtime: 1074 ms, faster than 24.80% of Python3 online submissions for Design HashSet.
-# Memory Usage: 41.3 MB, less than 5.31% of Python3 online submissions for Design HashSet.
+        return self.mem[key % self.size] == key
+
+
+# Runtime: 312 ms, faster than 48.75% of Python3 online submissions for Design HashSet.
+# Memory Usage: 21.4 MB, less than 21.37% of Python3 online submissions for Design HashSet.
