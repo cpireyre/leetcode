82b99a1479be5731c6137dc07d276e44ca86feb1
Colin <cpireyre@student.42.fr>
2022-04-23 10:13:05 +0200

    Runtime: 37 ms, faster than 78.47% of Python3 online submissions for Encode and Decode TinyURL. Memory Usage: 13.7 MB, less than 97.67% of Python3 online submissions for Encode and Decode TinyURL.

---
+++ encode-and-decode-tinyurl.py
@@ -0,0 +1,12 @@
+# no-op lol
+class Codec:
+
+    def encode(self, longUrl: str) -> str:
+        return longUrl
+        
+
+    def decode(self, shortUrl: str) -> str:
+        return shortUrl
+        
+# Runtime: 37 ms, faster than 78.47% of Python3 online submissions for Encode and Decode TinyURL.
+# Memory Usage: 13.7 MB, less than 97.67% of Python3 online submissions for Encode and Decode TinyURL.
